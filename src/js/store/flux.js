64a3abeb41a0be993de7216340194251d1c8c41f
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-07-05 23:47:05 +0000

    Adding API people data fetch and basic html/css

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,22 @@
+const { json } = require("react-router");
+
 const getState = ({ getStore, getActions, setStore }) => {
+
+	const loadData = async (serverUrl) => {
+		try{
+			const response = await fetch(serverUrl)
+			if (!response.ok){
+				console.log(response.ok)
+			}else{
+				const jsonData = await response.json();
+				console.log(jsonData)
+				return jsonData
+			}
+		}catch (error) {
+			console.log(error);
+		};
+	};
+
 	return {
 		store: {
 			demo: [
@@ -12,17 +30,54 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			people : []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			loadSomeData:async () => {
+
+				const serverUrl = "https://www.swapi.tech/api/people"
+				const unspecificApiData = await loadData(serverUrl)
+				console.log("unspecificApiData", unspecificApiData);
+				const peopleArr = []
+
+				for (const item of unspecificApiData.results){
+					console.log(item.url)
+					let temp = await loadData(item.url)
+					peopleArr.push (temp.result.properties)
+				};
+				console.log("DONE")
+				console.log(peopleArr)
+				setStore({people: peopleArr})
+
+				// const resultArray  = await Promise.all(unspecificApiData.map( async (item) => {
+				// 	loadData2(item.url)
+				// }));
+				// console.log(resultArray)
+			
+
+
+				// const serverUrl = "https://www.swapi.tech/api/people"
+				// fetch(serverUrl)
+				// .then(res => res.json())
+				// // .then(data => console.log(data))
+				// .then(data => {
+				// 	console.log(data)
+				// 	console.log(data.results)
+				// 	data.results.forEach( (item) => {
+				// 		console.log(item.url)
+
+				// 	})
+				// 	setStore({demo: data.results})
+				// })
+				// .catch(err => console.error(err))
+
+		
 			},
 			changeColor: (index, color) => {
 				//get the store
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,88 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	
+	// store.demo.forEach( (item, index ) => {
+	// 	console.log(item.name, index)
+	// });
+
+	return (
+		<div className="container">
+			<div className="row">
+				<h2>Characters</h2>
+			</div>
+			<ul className="list-group list-group-horizontal inline-scroll">
+				{store.people.map( (item, index) => {
+					return(
+						<li key={index}>
+							<div className="card" style={{width: "18rem", display: "inline-block", marginRight: "15px"}}>
+								<img src="..." className="card-img-top" alt="..."/>
+								<div className="card-body">
+									<h5 className="card-title">{item.name}</h5>
+									<p className="card-text">Gender: {item.gender}</p>
+									<p className="card-text">Hair color: {item.hair_color}</p>
+									<p className="card-text">Eye color: {item.eye_color}</p>
+									<a href="#" className="btn btn-primary">Learn more!</a>
+									<a href="#" className="btn btn-primary">Heart</a>
+								</div>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+
+			{/* <ul className="list-group">
+				{store.demo.map((item, index) => {
+					return (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between">
+							<span>Link to: {item.name}</span>
+						</li>
+					);
+				})}
+			</ul> */}
+		</div>
+	);
+
+	
+	{/* const { store, actions } = useContext(Context);
+	store.demo.forEach( (item, index ) => {
+		console.log(item, index)
+	}) */}
+
+	{/* return(
+		<div>
+			<div className="row">
+				<h2>Characters</h2>
+			</div>
+			<div className="row">
+				<div className="card" style={{width: "18rem"}}>
+					<img src="..." className="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+				<div className="card" style={{width: "18rem"}}>
+					<img src="..." className="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		); */}
+
+
+};
